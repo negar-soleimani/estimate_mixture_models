ebd9b31c36ab2de6d70a01b959dec8fc88da7df1
Negar Soleimani <negarsoleimani76@gmail.com>
2025-08-20 12:37:38 +0200

    prepare data

---
+++ data/prepare_data.R
@@ -0,0 +1,24 @@
+rm(list = ls())
+library(truncnorm)
+library(readxl)
+library(ggplot2)
+library(tidyr)
+library(MASS)
+library(mvtnorm)
+library(invgamma)
+
+don <- read_xlsx("E:/Phd_Paris Saclay/10Sep2024_Cours Calibration/Ball_drops_data.xlsx", sheet = 2)
+names(don) <- c("drop", "time", "Height", "Velocity")
+don$drop <- as.factor(don$drop)
+don <- don[don$drop == 1, ]
+
+
+
+
+t <- don$time
+y <- don$Height
+length(t)
+t_min <- min(t)
+t_range <- max(t) - min(t)
+t <- (t - t_min) / t_range
+n <- length(y)
