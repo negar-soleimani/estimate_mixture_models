86aba347a93cc5e0fe9c3dca350cb58c32b61cb4
Negar Soleimani <negarsoleimani76@gmail.com>
2025-08-21 12:27:20 +0200

    result

--- results/res.R
+++ results/res.R
@@ -63,79 +63,35 @@ print(prob_zeta_model1)
 ###################### Results for model 2 ######################################
 #################################################################################
 #################################################################################
-
-
-#psi5#########################################
-set.seed(12345)
-k <- 0.2
-sigma_sq_err <- 0.1
-sigma_sq_delta <- sigma_sq_err/k
-psi_delta_vec <-c(0.01,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9)
-n_samples <- 10
-n_iter <- 1000
-burn_in <- 100
-#y_1 <- y[,1]
-sigma_proposals <- c(NA, NA, NA, NA, 0.5, NA)
-# FALSE= fixed parameter
-# mcmc parameter (g,h), sig2err, psidelta, k, alpha
-#mcmc_parameters <- c(FALSE, TRUE, TRUE, TRUE, TRUE)
-Sigma_theta <- matrix(c(0.5, 0, 0, 0.5), nrow = 2)
-# c(g, h0, sig2err, alpha, psidelta, k)
-# h0 = 47.7, 46.45, 46.44, 48, 46.13
-init <- c(9.8, 46.45, 0.08, 0.7, 0.5, 0.2)
-
-g            <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-h0           <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-sigma_sq_err    <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-alpha        <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-psi_delta    <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-k            <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-loglik1      <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-delta1            <- list()
-zeta1            <- list()
-accept_rate  <- c()
-
-y_obs<- matrix(NA, length(t), n_samples, byrow = FALSE)
-for (v in 1:n_samples) {
-  Sigma_delta <- GP_covariance(t, sigma_sq_delta, psi_delta_vec[5])
-  delta<-rmvnorm(1,rep(3,n),Sigma_delta)
-  y_1 = balldropg(t,c(9.8,46.45)) + rnorm(length(t),0,sqrt(0.1)) + delta 
-  y_obs[,v]<- y_1
-  a_psi = 4
-  b_psi = 6
-  results <- mcmc_step6(y_1, t, n_iter, init, sigma_proposals, g_init = TRUE, h0_init= FALSE, sig2er_init = FALSE,
-                        alpha_init = FALSE, psi_init = FALSE, k_init = FALSE, Sigma_theta, n_burnin=burn_in, a_psi, b_psi, seuil = TRUE, s = 0.3)
+# Define a function to plot boxplots for each parameter
+plot_boxplot <- function(results_all, param_index, ylab, abline_value, ylim_value = NULL) {
+  results_list <- lapply(1:10, function(i) results_all[[i]][[param_index]])
   
-  g[,v] = results$theta[,1]
-  h0[,v]=results$theta[,2]
-  sigma_sq_err[,v]=results$theta[,3]
-  alpha[,v] <- results$theta[,4]
-  psi_delta[,v] <- results$theta[,5]
-  k[,v] <- results$theta[,6]
-  delta1[[v]] <-results$delta
-  zeta1[[v]] <- results$zeta
-  loglik1[, v] <- results$loglik
-  accept_rate[v] <- results$accept_rate_psi
+  par(mfrow = c(1,1), mai = c(1,1,0.5,0.5))
+  boxplot(lapply(results_list, colMeans), 
+          names = c("0.01","0.1","0.2","0.3","0.4","0.5","0.6","0.7","0.8","0.9"),
+          xlab = expression(psi[delta]),
+          ylab = ylab,
+          ylim = ylim_value,
+          cex.lab = 2, cex.axis = 2, cex.main = 2, cex.sub = 2)
+  abline(h = abline_value)
 }
 
-result_m2_sh2_psi5_new <- list(g, h0, sigma_sq_err, alpha, psi_delta, k, delta1, zeta1, loglik1, accept_rate)
+# Plot for g
+plot_boxplot(results_all, 1, expression(g), 9.8)
 
-g <- result_m2_sh2_psi5_new[[1]]
-h <- result_m2_sh2_psi5_new[[2]]
-sigma <- result_m2_sh2_psi5_new[[3]]
-alpha <- result_m2_sh2_psi5_new[[4]]
-psi <- result_m2_sh2_psi5_new[[5]]
-k <- result_m2_sh2_psi5_new[[6]]
+# Plot for h0
+plot_boxplot(results_all, 2, expression(h), 46.45, ylim_value = c(45, 50))
 
-par(mfrow=c(2,3))
-boxplot(g)
-abline(h=9.8)
-boxplot(h)
-abline(h=46.45)
-boxplot(sigma)
-abline(h=0.1)
-boxplot(alpha)
-boxplot(psi)
-abline(h=0.4)
-boxplot(k)
-abline(h=0.2)
+# Plot for sigma_sq_err
+plot_boxplot(results_all, 3, expression(sigma[err]^2), 0.1)
+
+# Plot for alpha
+plot_boxplot(results_all, 4, expression(alpha), 0.4, ylim_value = c(0, 1))
+
+# Plot for psi_delta
+plot_boxplot(results_all, 5, expression(psi[delta]), 0.4)
+
+# Plot for k
+plot_boxplot(results_all, 6, expression(k), 0.2)
+
