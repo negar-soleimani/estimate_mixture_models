3a3ca6a4ddcd43a5928a9e1404173b4f136de185
Negar Soleimani <negarsoleimani76@gmail.com>
2025-08-21 15:05:56 +0200

    result

--- results/res.R
+++ results/res.R
@@ -3,60 +3,279 @@
 ###################### Results for model 1 ######################################
 #################################################################################
 #################################################################################
-set.seed(12345)
-Sigma_theta <- matrix(c(0.1,0,0,0.1), nrow = 2)
-# c(g, h0, sig2err, alpha, psidelta, k)
-init <- c(9.8, 48, 0.08, 0.7, 0.5, 0.2)
-sigma_proposals <- c(NA, NA, NA, NA, 0.5, NA)
-n_samples       <- 3
-burn_in         <- 100
-n_iter          <- 1000
-# FALSE= fixed parameter
-# mcmc parameter (g,h), sig2err, psidelta, k, alpha
-mcmc_parameters <- c(TRUE, TRUE, TRUE, TRUE, TRUE)
+load("E:/Phd_Paris Saclay/resultat/Results_M1_Simulation/result_m1_sh2_presentation.RData")
+load("E:Phd_Paris Saclay/resultat/Results_M1_Simulation/y_obs_m1_sh2_presentation.RData")
+load("E:/Phd_Paris Saclay/resultat/Results_M1_Simulation/result_m1_sh1.RData")
+load("E:/Phd_Paris Saclay/resultat/Results_M1_Simulation/result_m1_sh2.RData")
+load("E:/Phd_Paris Saclay/resultat/Results_M1_Simulation/result_m1_sh3.RData")
+load("E:/Phd_Paris Saclay/resultat/Results_M1_Simulation/result_m1_sh4.RData")
+load("E:/Phd_Paris Saclay/resultat/Results_M1_Simulation/result_m1_sh5.RData")
 
-g            <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-h0           <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-sigma_sq_err <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-alpha        <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-psi_delta    <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-k            <- matrix(NA, n_iter, n_samples, byrow = FALSE)
+sheet_names <- c("Baseball",
+                 "Blue Basketball",
+                 "Green Basketball",
+                 "Volleyball",
+                 "Bowling Ball")
+
+#View(result_m1_sh1)
+
+g_sh1 <- result_m1_sh1[[1]]
+h0_sh1 <- result_m1_sh1[[2]]
+sigma_sq_err_sh1 <- result_m1_sh1[[3]]
+alpha_sh1 <- result_m1_sh1[[4]]
+psi_delta_sh1 <- result_m1_sh1[[5]]
+k_sh1 <- result_m1_sh1[[6]]
+
+g_sh2 <- result_m1_sh2[[1]]
+h0_sh2 <- result_m1_sh2[[2]]
+sigma_sq_err_sh2 <- result_m1_sh2[[3]]
+alpha_sh2 <- result_m1_sh2[[4]]
+psi_delta_sh2 <- result_m1_sh2[[5]]
+k_sh2 <- result_m1_sh2[[6]]
+
+g_sh3 <- result_m1_sh3[[1]]
+h0_sh3 <- result_m1_sh3[[2]]
+sigma_sq_err_sh3 <- result_m1_sh3[[3]]
+alpha_sh3 <- result_m1_sh3[[4]]
+psi_delta_sh3 <- result_m1_sh3[[5]]
+k_sh3 <- result_m1_sh3[[6]]
+
+g_sh4 <- result_m1_sh4[[1]]
+h0_sh4 <- result_m1_sh4[[2]]
+sigma_sq_err_sh4 <- result_m1_sh4[[3]]
+alpha_sh4 <- result_m1_sh4[[4]]
+psi_delta_sh4 <- result_m1_sh4[[5]]
+k_sh4 <- result_m1_sh4[[6]]
+
+g_sh5 <- result_m1_sh5[[1]]
+h0_sh5 <- result_m1_sh5[[2]]
+sigma_sq_err_sh5 <- result_m1_sh5[[3]]
+alpha_sh5 <- result_m1_sh5[[4]]
+psi_delta_sh5 <- result_m1_sh5[[5]]
+k_sh5 <- result_m1_sh5[[6]]
+
+g_sh2 <- result_m1_sh2_presentation[[1]]
+h0_sh2 <- result_m1_sh2_presentation[[2]]
+sigma_sq_err_sh2 <- result_m1_sh2_presentation[[3]]
+alpha_sh2 <- result_m1_sh2_presentation[[4]]
+psi_delta_sh2 <- result_m1_sh2_presentation[[5]]
+k_sh2 <- result_m1_sh2_presentation[[6]]
+
+par(mfrow = c(1, 3),
+    mar   = c(3, 4, 1, 1) 
+)
+boxplot(
+  colMeans(g_sh2),
+  ylab = "g",
+  #xlab = "Bowling Ball",
+  col  = "#800020",
+  main = NULL
+)
+abline(h = 9.8, lty = 2)
+
+boxplot(
+  colMeans(h0_sh2),
+  ylab = "h0",
+  #xlab = "",
+  col  = "#800020",
+  main = NULL
+)
+abline(h = 46.452, lty = 2)
+
+boxplot(
+  colMeans(sigma_sq_err_sh2),
+  ylab = expression(sigma[err]^2),
+  #xlab = "",
+  col  = "#800020",
+  main = NULL
+)
+abline(h = 0.0537, lty = 2)
 
 
-y_obs <- matrix(NA, length(t), n_samples, byrow = FALSE)
-for (v in 1:n_samples) {
-  y_1 = balldropg(t,c(9.8, 46.45)) + rnorm(n,0,.1)
-  y_obs[,v] <- y_1
-  
-  results <- mcmc_step6(y_1, t, n_iter, init, sigma_proposals, mcmc_parameters, Sigma_theta, n_burnin = burn_in)
-  
-  g[,v] = results$theta[,1]
-  h0[,v]=results$theta[,2]
-  sigma_sq_err[,v]=results$theta[,3]
-  alpha[,v] <- results$theta[,4]
-  psi_delta[,v] <- results$theta[,5]
-  k[,v] <- results$theta[,6]
+boxplot(
+  colMeans(alpha_sh5),
+  ylab = expression(alpha),
+  xlab = "",
+  col  = "#800020",
+  main = NULL
+)
+abline(h = 0.7, lty = 2)
+
+boxplot(
+  colMeans(psi_delta_sh5),
+  ylab = expression(psi[delta]),
+  xlab = "",
+  col  = "#800020",
+  main = NULL
+)
+abline(h = 0.3, lty = 2)
+
+boxplot(
+  colMeans(k_sh5),
+  ylab = "k",
+  xlab = "",
+  col  = "#800020",
+  main = NULL
+)
+abline(h = 0.2, lty = 2)
+
+don <- read_xlsx("E:/Phd_Paris Saclay/10Sep2024_Cours Calibration/Ball_drops_data.xls", sheet = 2)
+names(don) <- c("drop", "time", "Height", "Velocity")
+don$drop <- as.factor(don$drop)
+don <- don[don$drop == 1, ]
+
+t <- don$time
+y <- don$Height
+t_min <- min(t)
+t_range <- max(t) - min(t)
+t <- (t - t_min) / t_range
+n <- length(y)
+
+balldropg <- function(t, theta) {
+  g <- theta[1]
+  h0 <- theta[2]
+  theta_vec <- rbind(h0, g)
+  x_vec <- cbind(1, -0.5 * (t * t_range + t_min)^2)
+  h <- x_vec %*% theta_vec
+  h[h < 0] <- 0
+  return(as.vector(h))
 }
 
-result_m1_sh2_presentation <- list(g, h0, sigma_sq_err, alpha, psi_delta, k, y_obs = y_obs)
-y_obs_m1_sh2_presentation <- y_obs
+g_mat <- result_m1_sh2_presentation[[1]]   # n_iter × n_sims
+h0_mat<- result_m1_sh2_presentation[[2]]
 
-g_mean <- colMeans(g)
-boxplot(g_mean)
-h0_mean <- colMeans(h0)
-boxplot(h0_mean)
-sigma_sq_err_mean <- colMeans(sigma_sq_err)
-boxplot(sigma_sq_err_mean)
-alpha_mean <- colMeans(alpha)
-boxplot(alpha_mean)
-psi_delta_mean <- colMeans(psi_delta)
-boxplot(psi_delta_mean)
-k_mean <- colMeans(k)
-boxplot(k_mean)
+g_last <- g_mat[, 30]
+h0_last <- h0_mat[, 30]
+y_obs <- result_m1_sh2_presentation$y_obs
+y_obs30 <- y_obs[,30]
+y_true <- balldropg(t, c(9.8, 46.45))
 
+y_pred <- matrix(NA, length(t), length(g_last), byrow = FALSE)
 
-prob_zeta_model1 <- colMeans(results$zeta == 1)
-print(prob_zeta_model1)
+for (i in 1:length(g_last)) {
+  theta <- c(g_last[i], h0_last[i])
+  y_pred[,i] <- balldropg(t, theta)
+}
+
+y_pred1  = t(y_pred)
+par(mfrow = c(1,1))
+boxplot(y_pred1, col = "orange2",
+        xlab = "Time", ylab = "Height")
+lines(y_true, lwd = 2, col = "blue")
+points(y_obs30, col = "gold", pch=20, cex = 1)
+legend(x=30, y=45, legend=c("Simulated data",
+                            "True code", "Predictions"), lwd=rep(2,2), col=c("gold","blue", "orange2"), 
+       cex=1, pch=c(19,NA,NA), lty=c(0,1,1))
+
+boxplot(y_pred1[, 1:5], col = "orange2",
+        xlab = "Time", ylab = "Height")
+lines(y_true, lwd = 2, col = "blue")
+points(y_obs30, col = "gold", pch=20, cex = 3)
+legend(x=30, y=45, legend=c("Simulated data", "Predictions",
+                            "True code"), lwd=rep(2,2), col=c("gold", "orange2","blue"), 
+       cex=1, pch=c(19,NA,NA), lty=c(0,1,1))
+
+y_pred2 <- rowMeans(y_pred) 
+y_obs2 <- rowMeans(y_obs)
+
+df_cmp <- data.frame(
+  time   = t,
+  y_obs2  = y_obs2,
+  y_pred2  = y_pred2,
+  y_true = y_true
+)
+library(ggplot2)
+p <- ggplot(df_cmp, aes(x = time)) +
+  geom_point(aes(y = y_obs2, color = "Observed"),      size = 1) +
+  geom_line( aes(y = y_pred2,  color = "Estimate"),     linetype = "dashed", size = 0.5) +
+  geom_line( aes(y = y_true, color = "True"),         linetype = "solid",  size = 0.5) +
+  scale_color_manual(
+    name   = NULL,
+    values = c("Observed" = "#800020",
+               "Estimate" = "green",
+               "True"     = "blue")
+  ) +
+  theme_minimal(base_size = 14) +
+  labs(
+    x     = "Time",
+    y     = ""
+  ) +
+  theme(
+    legend.position      = c(0.95, 0.95),
+    legend.justification = c("right", "top"),
+    legend.background    = element_rect(fill = alpha("white", 0.6)),
+    legend.key           = element_rect(fill = NA)
+  )
+
+print(p)
+
+boxplot(colMeans(g_sh1), colMeans(g_sh2), colMeans(g_sh3), colMeans(g_sh4), colMeans(g_sh5),
+        names  = sheet_names,
+        ylab = "g",
+        col    = rainbow(5))
+
+boxplot(colMeans(h0_sh1), colMeans(h0_sh2), colMeans(h0_sh3), colMeans(h0_sh4), colMeans(h0_sh5),
+        names  = sheet_names,
+        ylab = "h0",
+        col    = rainbow(5))
+
+boxplot(colMeans(sigma_sq_err_sh1), colMeans(sigma_sq_err_sh2), colMeans(sigma_sq_err_sh3), 
+        colMeans(sigma_sq_err_sh4), colMeans(sigma_sq_err_sh5),
+        names  = sheet_names,
+        ylab = "sigma_sq_err",
+        col    = rainbow(5))
+
+boxplot(colMeans(alpha_sh1), colMeans(alpha_sh2), colMeans(alpha_sh3), colMeans(alpha_sh4), 
+        colMeans(alpha_sh5),
+        names  = sheet_names,
+        ylab = "alpha",
+        ylim = c(0.4,1),
+        col    = rainbow(5))
+
+boxplot(colMeans(psi_delta_sh1), colMeans(psi_delta_sh2), colMeans(psi_delta_sh3), 
+        colMeans(psi_delta_sh4), colMeans(psi_delta_sh5),
+        names  = sheet_names,
+        ylab = "psi_delta",
+        col    = rainbow(5))
+
+boxplot(colMeans(k_sh1), colMeans(k_sh2), colMeans(k_sh3), colMeans(k_sh4), colMeans(k_sh5),
+        names  = sheet_names,
+        ylab = "k",
+        col    = rainbow(5))
+
+load("E:/Phd_Paris Saclay/resultat/Results_M1_Simulation/result_m1_sh2_presentation.RData")
+load("E:Phd_Paris Saclay/resultat/Results_M1_Simulation/y_obs_m1_sh2_presentation.RData")
+
+alpha <- result_m1_sh2_presentation[[4]]
+hist(alpha)
+plot(density(alpha))
+
+library(ggplot2)
+
+# 1) flatten the matrix to a single vector
+alpha_vec <- as.vector(alpha)
+
+# 2) put into a data.frame
+df_alpha <- data.frame(alpha = alpha_vec)
+
+# 3) ggplot density
+ggplot(df_alpha, aes(x = alpha)) +
+  geom_density(
+    fill  = "#800020",   # burgundy fill
+    color = "#550010",   # darker border
+    alpha = 0.6,         # semi‐transparent
+    size  = 1
+  ) +
+  labs(
+    x     = expression(alpha),
+    y     = "Density",
+    title = "Posterior Density of " ~ alpha
+  ) +
+  theme_minimal(base_size = 14) +
+  theme(
+    plot.title   = element_text(hjust = 0.5),
+    plot.margin  = unit(c(0.2,0.2,0.2,0.2), "cm")
+  )
 
 #################################################################################
 #################################################################################
