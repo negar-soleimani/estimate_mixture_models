65140175070bfd4ffe029afd828fea611f595a88
Negar Soleimani <negarsoleimani76@gmail.com>
2025-11-07 11:46:37 +0100

    add poster code

--- code_poster.R
+++ code_poster.R
@@ -27,7 +27,7 @@ balldropg <- function(t, theta) {
   theta_vec <- rbind(h0, g)
   x_vec <- cbind(1, -0.5 * (t * t_range)^2) #cbind(1, -0.5 * (t * t_range + t_min)^2)
   h <- x_vec %*% theta_vec
-  #h[h < 0] <- 0
+  h[h < 0] <- 0
   return(as.vector(h))
 }
 
