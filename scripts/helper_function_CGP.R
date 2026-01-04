6144cd862d36aba8cc6725a65fea0be84efe3f15
Negar Soleimani <negarsoleimani76@gmail.com>
2026-01-04 20:04:46 +0100

    modify

--- scripts/helper_function_CGP.R
+++ scripts/helper_function_CGP.R
@@ -1,3 +1,5 @@
+# helper function for when we want to use the simple exponential kernel function in the covariance matrix related to the model discrepancy
+
 GP_covariance <- function(t, sigma_sq_delta, psi_delta) {
   n <- length(t)
   Sigma <- outer(t, t, function(ti, tj) sigma_sq_delta * exp(-abs(ti - tj) / psi_delta))
