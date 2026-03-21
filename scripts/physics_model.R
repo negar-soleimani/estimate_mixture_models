8bd27f4c7acc1c5e868d95822c742aabd7076bea
Negar Soleimani <negar@mia-ps-port071.local>
2026-03-21 15:47:06 +0100

    modify

--- scripts/physics_model.R
+++ scripts/physics_model.R
@@ -2,7 +2,7 @@ balldropg <- function(t, theta) {
   g <- theta[1]
   h0 <- theta[2]
   theta_vec <- rbind(h0, g)
-  x_vec <- cbind(1, -0.5 * (t * t_range + t_min)^2)  #cbind(1, -0.5 * (t * t_range)^2) #
+  x_vec <- cbind(1, -0.5 * (t * t_range + t_min)^2) #cbind(1, -0.5 * (t * t_range)^2)
   h <- x_vec %*% theta_vec
   h[h < 0] <- 0
   return(as.vector(h))
