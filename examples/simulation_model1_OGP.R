9cc26fb36205b9013ea83b8e4074bec217abf4d5
Negar Soleimani <negar@mia-ps-port071.local>
2026-05-26 10:22:10 +0200

    modify

--- examples/simulation_model1_OGP.R
+++ examples/simulation_model1_OGP.R
@@ -6,6 +6,64 @@ source("scripts/helper_function_OGP.R")
 source("scripts/helper_function_CGP.R")
 source("scripts/main_function_OGP.R")
 
+# ## Psi1 ####################################################
+set.seed(12345)
+k = 0.1
+#sigma_sq_delta <- 0.1
+sim_psi_delta <- 0.5
+sigma_sq_err <- 0.01
+sigma_sq_delta <- sigma_sq_err / k
+n_samples <- 5
+n_iter <- 10000
+burn_in <- 2000
+n <- length(y)
+# 
+sigma_proposals <- c(NA, NA, NA, NA, 0.5, NA)
+#(g,h0), sigma, psi, k, alpha
+#mcmc_parameters <- c(TRUE, TRUE, TRUE, TRUE, TRUE)
+Sigma_theta <- matrix(c(0.5, 0, 0, 0.5), nrow = 2)
+init <- c(9.8, 46.46, 0.01, 0.5, 0.2, 0.1)
+# 
+g_chain     <- matrix(NA, n_iter, n_samples)
+h0_chain    <- matrix(NA, n_iter, n_samples)
+sigma_chain <- matrix(NA, n_iter, n_samples)
+alpha_chain <- matrix(NA, n_iter, n_samples)
+psi_chain   <- matrix(NA, n_iter, n_samples)
+k_chain     <- matrix(NA, n_iter, n_samples)
+loglik_mat  <- matrix(NA, n_iter, n_samples)
+delta_list  <- vector("list", n_samples)
+zeta_list   <- vector("list", n_samples)
+accept_rate <- numeric(n_samples)
+# 
+y_obs <- matrix(NA, n, n_samples)
+for (v in 1:n_samples) {
+  Sigma_delta <- GP_covariance(t, sigma_sq_delta, sim_psi_delta)
+  
+  delta <- as.vector(rmvnorm(1, rep(0, n), Sigma_delta))
+  
+  y_1 <- balldropg(t, c(9.8, 46.46)) + rnorm(n, 0, sqrt(sigma_sq_err)) + delta
+  y_obs[, v] <- y_1
+  #res <- mcmc_step6(y_1, t, n_iter, init, sigma_props, mcmc_parameters,
+  #                  Sigma_theta, n_burnin = burn_in)
+  res <- mcmc_step6(y, t, n_iter, init, sigma_proposals,
+                    g_init=FALSE, h0_init= FALSE, sig2er_init = FALSE,
+                    alpha_init = FALSE, psi_init = FALSE, k_init = FALSE, Sigma_theta, n_burnin=1000)
+  g_chain[, v]     <- res$theta[, 1]
+  h0_chain[, v]    <- res$theta[, 2]
+  sigma_chain[, v] <- res$theta[, 3]
+  alpha_chain[, v] <- res$theta[, 4]
+  psi_chain[, v]   <- res$theta[, 5]
+  k_chain[, v]     <- res$theta[, 6]
+  delta_list[[v]]  <- res$delta
+  zeta_list[[v]]   <- res$zeta
+  loglik_mat[, v]  <- res$loglik
+  accept_rate[v]   <- res$accept_rate_psi
+}
+
+result_m2_sh2_psi1_ortho_ex <- list(g_chain, h0_chain, sigma_chain, alpha_chain, psi_chain, k_chain, delta_list, zeta_list, loglik_mat, accept_rate)
+#save(result_m2_sh2_psi1_ortho,file = "/Users/negarsoleimani/Documents/phd/paper1/github/model1/orthogonalgp/result_m2_sh2_psi1_ortho.RData")
+
+
 #############################################################
 # Simulation the data of model 2 with different psi_delta
 # ## Psi1 ####################################################
