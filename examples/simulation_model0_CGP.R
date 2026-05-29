66513074179515b3619ee41792e329c4e88df389
Negar Soleimani <negarsoleimani76@gmail.com>
2026-05-29 06:05:32 +0200

    modify

--- examples/simulation_model0_CGP.R
+++ examples/simulation_model0_CGP.R
@@ -1,9 +1,4 @@
 source("data/prepare_data.R")
-<<<<<<< HEAD
-source("scripts/helper_function_CGP.R")
-source("scripts/main_function_CGP.R")
-source("scripts/physics_model.R")
-=======
 rm(list = ls())
 library(truncnorm)
 library(readxl)
@@ -39,109 +34,58 @@ a <- t_range
 
 source("scripts/physics_model.R")
 source("scripts/helper_function_CGP.R")
-source("scripts/main_function_seuil_CGP.R")
->>>>>>> 8450abc4c47461f1601519d1d05674d6497dd7ed
+source("scripts/main_function_CGP.R")
 
 set.seed(12345)
 Sigma_theta <- matrix(c(0.5,0,0,0.5), nrow = 2)
 # c(g, h0, sig2err, alpha, psidelta, k)
-<<<<<<< HEAD
-init <- c(9.8, 46.45, 0.01, 0.5, 0.5, 0.2)
-=======
 init <- c(9.8, 46.45, 0.01, 0.5, 0.5, 0.1)
->>>>>>> 8450abc4c47461f1601519d1d05674d6497dd7ed
 sigma_proposals <- c(NA, NA, NA, NA, 0.5, NA)
 n_samples       <- 50
 burn_in         <- 2000
 n_iter          <- 10000
 # FALSE= fixed parameter
 # mcmc parameter (g,h), sig2err, psidelta, k, alpha, freeze delta-zeta
-<<<<<<< HEAD
-mcmc_parameters <- c(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE)
+# mcmc_parameters <- c(TRUE, TRUE, TRUE, TRUE, TRUE, FALSE)
+# 
+# g            <- matrix(NA, n_iter, n_samples, byrow = FALSE)
+# h0           <- matrix(NA, n_iter, n_samples, byrow = FALSE)
+# sigma_sq_err <- matrix(NA, n_iter, n_samples, byrow = FALSE)
+# alpha        <- matrix(NA, n_iter, n_samples, byrow = FALSE)
+# psi_delta    <- matrix(NA, n_iter, n_samples, byrow = FALSE)
+# k            <- matrix(NA, n_iter, n_samples, byrow = FALSE)
+# loglik_mat   <- matrix(NA, n_iter, n_samples)
+# delta_list   <- vector("list", n_samples)
+# zeta_list    <- vector("list", n_samples)
+# accept_rate  <- numeric(n_samples)
 
-g            <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-h0           <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-sigma_sq_err <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-alpha        <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-psi_delta    <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-k            <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-
-
-y_obs <- matrix(NA, length(t), n_samples, byrow = FALSE)
-for (v in 1:n_samples) {
-  y_1 = balldropg(t,c(9.8, 46.45)) + rnorm(n, 0, sqrt(0.01))
-  y_obs[,v] <- y_1
-  
-  #results <- mcmc_step6(y_1, t, n_iter, init, sigma_proposals, g_init=FALSE, h0_init= FALSE, sig2er_init = FALSE,
-  #                      alpha_init = FALSE, psi_init = FALSE, k_init = FALSE, Sigma_theta, n_burnin = burn_in, a_psi, b_psi, seuil = FALSE, s = 0.3)
-  results <- mcmc_step6(y_1, t, n_iter, init, sigma_proposals, mcmc_parameters, Sigma_theta, n_burnin=2000)
-=======
-#mcmc_parameters <- c(TRUE, TRUE, TRUE, TRUE, TRUE, FALSE)
-
-#g            <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-#h0           <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-#sigma_sq_err <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-#alpha        <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-#psi_delta    <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-#k            <- matrix(NA, n_iter, n_samples, byrow = FALSE)
-#loglik_mat   <- matrix(NA, n_iter, n_samples)
-#delta_list   <- vector("list", n_samples)
-#zeta_list    <- vector("list", n_samples)
-#accept_rate  <- numeric(n_samples)
-
-#y_obs <- matrix(NA, length(t), n_samples, byrow = FALSE)
-#for (v in 1:n_samples) {
-#  y_1 = balldropg(t,c(9.8, 46.45)) + rnorm(n, 0, sqrt(0.01))
-#  y_obs[,v] <- y_1
-  
-<<<<<<< HEAD
-  results <- mcmc_step6(y_1, t, n_iter, init, sigma_proposals,
-                        g_init=FALSE, h0_init= FALSE, sig2er_init = FALSE,
-                        alpha_init = FALSE, psi_init = FALSE, k_init = FALSE, Sigma_theta, n_burnin=1000, seuil = FALSE, s = 0.3)
-  #results <- mcmc_step6(y_1, t, n_iter, init, sigma_proposals, mcmc_parameters, Sigma_theta, n_burnin=2000)
->>>>>>> 8450abc4c47461f1601519d1d05674d6497dd7ed
-  g[,v] = results$theta[,1]
-  h0[,v]=results$theta[,2]
-  sigma_sq_err[,v]=results$theta[,3]
-  alpha[,v] <- results$theta[,4]
-  psi_delta[,v] <- results$theta[,5]
-  k[,v] <- results$theta[,6]
-<<<<<<< HEAD
-}
-
-result_m1_sh2 <- list(g, h0, sigma_sq_err, alpha, psi_delta, k, y_obs = y_obs)
-y_obs_m1_sh2 <- y_obs
-=======
-  delta_list[[v]]  <- results$delta
-  zeta_list[[v]]   <- results$zeta
-  loglik_mat[, v]  <- results$loglik
-  accept_rate[v]   <- results$accept_rate_psi
-}
-=======
-#  results <- mcmc_step6(y_1, t, n_iter, init, sigma_proposals,
-#                        g_init=FALSE, h0_init= FALSE, sig2er_init = FALSE,
-#                        alpha_init = FALSE, psi_init = FALSE, k_init = FALSE, Sigma_theta, n_burnin=1000, seuil = FALSE, s = 0.3)
-#  #results <- mcmc_step6(y_1, t, n_iter, init, sigma_proposals, mcmc_parameters, Sigma_theta, n_burnin=2000)
-#  g[,v] = results$theta[,1]
-#  h0[,v]=results$theta[,2]
-#  sigma_sq_err[,v]=results$theta[,3]
-#  alpha[,v] <- results$theta[,4]
-#  psi_delta[,v] <- results$theta[,5]
-#  k[,v] <- results$theta[,6]
-#  delta_list[[v]]  <- results$delta
-#  zeta_list[[v]]   <- results$zeta
-#  loglik_mat[, v]  <- results$loglik
-#  accept_rate[v]   <- results$accept_rate_psi
-#}
->>>>>>> c4a5a55177a64d784bf447d086806725ce879dc9
+# y_obs <- matrix(NA, length(t), n_samples, byrow = FALSE)
+# for (v in 1:n_samples) {
+#   y_1 = balldropg(t,c(9.8, 46.45)) + rnorm(n, 0, sqrt(0.01))
+#   y_obs[,v] <- y_1
+#   
+#   #results <- mcmc_step6(y_1, t, n_iter, init, sigma_proposals, g_init=FALSE, h0_init= FALSE, sig2er_init = FALSE,
+#   #                      alpha_init = FALSE, psi_init = FALSE, k_init = FALSE, Sigma_theta, n_burnin = burn_in, a_psi, b_psi, seuil = FALSE, s = 0.3)
+#   results <- mcmc_step6(y_1, t, n_iter, init, sigma_proposals, mcmc_parameters, Sigma_theta, n_burnin=2000)
+#   g[,v] = results$theta[,1]
+#   h0[,v]=results$theta[,2]
+#   sigma_sq_err[,v]=results$theta[,3]
+#   alpha[,v] <- results$theta[,4]
+#   psi_delta[,v] <- results$theta[,5]
+#   k[,v] <- results$theta[,6]
+#   delta_list[[v]]  <- results$delta
+#   zeta_list[[v]]   <- results$zeta
+#   loglik_mat[, v]  <- results$loglik
+#   accept_rate[v]   <- results$accept_rate_psi
+# }
 
 #result_m0_sh2_classic_classic_ex <- list(g, h0, sigma_sq_err, alpha, psi_delta, k, y_obs = y_obs, delta_list, zeta_list, loglik_mat, accept_rate)
 
 #result_m0_sh2_classic_classic_200 <- list(g, h0, sigma_sq_err, alpha, psi_delta, k, y_obs = y_obs, delta_list, zeta_list, loglik_mat, accept_rate)
 #save(result_m0_sh2_classic_classic_200,file = "/Users/negar/Documents/phd/Result/Model1/Classic/result_m0_sh2_classic_classic_200.RData")
 
-#result_m0_sh2_classic_classic_100 <- list(g, h0, sigma_sq_err, alpha, psi_delta, k, y_obs = y_obs, delta_list, zeta_list, loglik_mat, accept_rate)
-#save(result_m0_sh2_classic_classic_100,file = "/Users/negar/Documents/phd/Result/Model1/Classic/result_m0_sh2_classic_classic_100.RData")
+# result_m0_sh2_classic_classic_100 <- list(g, h0, sigma_sq_err, alpha, psi_delta, k, y_obs = y_obs, delta_list, zeta_list, loglik_mat, accept_rate)
+# save(result_m0_sh2_classic_classic_100,file = "/Users/negar/Documents/phd/Result/Model1/Classic/result_m0_sh2_classic_classic_100.RData")
 
 #result_m0_sh2_classic_classic <- list(g, h0, sigma_sq_err, alpha, psi_delta, k, y_obs = y_obs, delta_list, zeta_list, loglik_mat, accept_rate)
 #save(result_m0_sh2_classic_classic,file = "/Users/negar/Documents/phd/Result/Model1/Classic/result_m0_sh2_classic_classic.RData")
